2e3ee26dde4d0b588a5990d8508f7c8f116b148d
Dzmitry Ryzhou <95518976+Demetrius81@users.noreply.github.com>
2024-11-05 09:15:50 +0300

    feat: implements getSumToN

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -181,8 +181,8 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -202,8 +202,10 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  return ![...Array(n).keys()]
+    .map((x, index) => !(n % x === 0 && index > 1))
+    .includes(false, 2);
 }
 
 /**
@@ -221,8 +223,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return value === null || Number.isNaN(+value) ? def : +value;
 }
 
 /**
@@ -236,8 +238,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
@@ -253,8 +255,15 @@ function getCube(/* num */) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index <= 0) {
+    return 0;
+  }
+  if (index === 1) {
+    return 1;
+  }
+
+  return getFibonacciNumber(index - 1) + getFibonacciNumber(index - 2);
 }
 
 /**
@@ -268,8 +277,16 @@ function getFibonacciNumber(/* index */) {
  *   10 => 55 // (1+2+3+...+10)
  *   1  => 1
  */
-function getSumToN(/* n */) {
-  throw new Error('Not implemented');
+function getSumToN(n) {
+  return [...Array((n < 0 ? 0 : n) + 1).keys()].reduce((elem, acc) => {
+    let res = acc;
+    res += elem;
+    return res;
+  }, 0);
+  // return [...Array((n < 0 ? 0 : n) + 1).keys()].reduce(
+  //   (elem, acc) => (acc += elem),
+  //   0
+  // );
 }
 
 /**
